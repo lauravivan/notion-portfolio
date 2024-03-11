4939641851bd63e7a6332ccddd5bd3c949d2f539
lauravivan <lauvivang@gmail.com>
2024-03-11 18:54:00 -0300

    Add alias to folders in shared folder

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,15 @@ import VitePages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePages({
-      pagesDir: "src/pages",
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": "/src",
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: "/index.html",
+      components: "/src/shared/components",
+      hooks: "/src/shared/hooks",
+      store: "/src/shared/store/store.js",
+      UIElements: "/src/shared/UIElements",
+      util: "/src/shared/util",
     },
   },
 });
