a2b1d0d177f2880bad60af7c3a701c7a40506c71
lauravivan <lauvivang@gmail.com>
2024-04-23 16:18:32 -0300

    Added page for projects

---
+++ src/pages/Projects.vue
@@ -0,0 +1,16 @@
+<template>
+  <Empty />
+  <Gallery
+    :galleryTitle="'My Projects'"
+    :pages="activePage.pages"
+    :cardPreviewIsCover="true"
+    :component="Project"
+  />
+</template>
+
+<script setup>
+import Gallery from "components/Gallery.vue";
+import Empty from "UIElements/Empty.vue";
+import { activePage } from "global";
+import Project from "@/pages/Project.vue";
+</script>
