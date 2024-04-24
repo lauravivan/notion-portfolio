3fb59988497667dff32cc0dcac6a1f18d53a8e19
lauravivan <lauvivang@gmail.com>
2024-04-24 15:38:49 -0300

    Add adjustments for database page and update layout

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,4 +1,13 @@
 <template>
+  <div v-if="page">
+    <Database
+      v-if="!props.hideDatabase"
+      :multiSelectItems="page.pageData.languages"
+    >
+      <template #dateTimeDesc>Created</template>
+      <template #multiSelectDesc>Used languages</template>
+    </Database>
+  </div>
   <div class="project" v-if="page">
     <Text
       >Access here:
@@ -11,9 +20,6 @@
       <Heading headingNumber="3"> Preview: </Heading>
       <iframe class="iframe" :src="page.pageData.homepage"></iframe>
     </div>
-
-    <Text>Languages used:</Text>
-    <BulletList :items="page.pageData.languages" />
   </div>
 </template>
 
@@ -22,10 +28,9 @@ import { activePage } from "global";
 import Database from "components/Database.vue";
 import Text from "UIElements/Text.vue";
 import Heading from "UIElements/Heading.vue";
-import BulletList from "components/BulletList.vue";
 import { ref, onMounted } from "vue";
 
-const props = defineProps(["page"]);
+const props = defineProps(["page", "hideDatabase"]);
 const page = ref(null);
 
 onMounted(() => {
@@ -40,42 +45,18 @@ onMounted(() => {
 <style lang="scss">
 @import "@/assets/scss/main.scss";
 
-.project {
-  &__link {
-    margin-top: 30px;
-    border-bottom: 1px solid $gray-9;
-    padding-bottom: 20px;
-  }
-}
 .iframe-title {
   font-size: $fs-xl;
   font-weight: $fw-500;
 }
+
 .iframe-wrapper {
   @include flex-layout($row-gap: 10px);
   margin: 60px 0;
 }
+
 .iframe {
   width: 100%;
   height: 330px;
-}
-
-.languages-wrapper {
-  margin-top: 100px;
-  @include flex-layout($row-gap: 40px);
-
-  > span {
-    font-size: $fs-large;
-    text-decoration: underline;
-    font-weight: $fw-500;
-  }
-}
-
-.languages {
-  @include flex-layout($flex-direction: row, $column-gap: 10px);
-}
-
-.language {
-  @include language($baby-blue);
 }
 </style>
---
+++ src/pages/Repo.vue
@@ -0,0 +1,74 @@
+<template>
+  <div v-if="page">
+    <Database
+      v-if="!props.hideDatabase"
+      :multiSelectItems="page.pageData.languages"
+    >
+      <template #dateTimeDesc>Created</template>
+      <template #multiSelectDesc>Used languages</template>
+    </Database>
+  </div>
+  <div class="repo" v-if="page">
+    <div class="repo__metrics">
+      <div class="repo__metric">
+        <Icon :icon="icons.star" />
+        <div>{{ page.pageData.starsCount }}</div>
+      </div>
+      <div class="repo__metric">
+        <Icon :icon="icons.eye" />
+        <div>{{ page.pageData.watchersCount }}</div>
+      </div>
+      <div class="repo__metric">
+        <Icon :icon="icons.forkCode" />
+        <div>{{ page.pageData.forksCount }}</div>
+      </div>
+    </div>
+    <Empty />
+    <Text><span>Description: </span>{{ page.pageData.description }}</Text>
+    <Empty />
+    <Text style="display: flex; column-gap: 10px">
+      <Icon :icon="icons.code" />
+      <span class="highlight">Link github: </span
+      ><a :href="page.pageData.gitUrl">{{ page.pageData.gitUrl }}</a>
+    </Text>
+    <Text v-if="page.pageData.homepage" style="display: flex; column-gap: 10px">
+      <Icon :icon="icons.link" />
+      <span class="highlight">Link page: </span
+      ><a :href="page.pageData.gitUrl">{{ page.pageData.homepage }}</a>
+    </Text>
+  </div>
+</template>
+
+<script setup>
+import Database from "components/Database.vue";
+import Icon from "UIElements/Icon.vue";
+import { icons, activePage } from "global";
+import Text from "UIElements/Text.vue";
+import Empty from "UIElements/Empty.vue";
+import { ref, onMounted } from "vue";
+
+const props = defineProps(["page", "hideDatabase"]);
+const page = ref(null);
+
+onMounted(() => {
+  if (props.page) {
+    page.value = props.page;
+  } else {
+    page.value = activePage.value;
+  }
+});
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/main.scss";
+
+.repo {
+  &__metrics {
+    @include flex-layout($flex-direction: row, $column-gap: 15px);
+  }
+
+  &__metric {
+    @include flex-layout($flex-direction: row, $column-gap: 10px);
+  }
+}
+</style>
