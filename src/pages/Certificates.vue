d6c1d491a7dc6b287449e9755fbfeaaf8c29e496
lauravivan <lauvivang@gmail.com>
2025-09-22 16:53:51 -0300

    Delete certificates and update structure

--- src/pages/Certificates.vue
+++ src/pages/Certificates.vue
@@ -1,59 +1,100 @@
 <script setup lang="ts">
 import Bookmark from "@/components/Bookmark.vue";
+import Column from "@/components/Column.vue";
+import Text from "@/components/Text.vue";
+import Link from "@/components/Link.vue";
+import { useStore } from "vuex";
+import { computed } from "vue";
+
+const store = useStore();
+const theme = computed(() => store.getters.getTheme);
 </script>
 
 <template>
   <div class="certificates">
-    <Bookmark
-      title="Desenvolvimento Front-End do Zero ao Pro"
-      subtitle="EBAC - 11956-70519-75448-83098"
-      link="https://static.lms.ebaconline.com.br/certs/115b328c-e890-4879-a789-622e2b59f84d.pdf"
-    />
-    <Bookmark
-      title="JavaScript Unit Testing - The Practical Guide"
-      subtitle="Udemy - UC-3efc1286-ed24-496e-8939-a95f8c35e4cb"
-      link="https://www.udemy.com/certificate/UC-3efc1286-ed24-496e-8939-a95f8c35e4cb/"
-    />
-    <Bookmark
-      title="Meetup Codecon Ladies"
-      subtitle="Codecon - 28332382.05162310.5.8.87727823659275368"
-      link="https://eventos.codecon.dev/documentos/imprimir?i=28332382.05162310.5.8.87727823659275368&cc=A6530560-1A59-43F5-B1E8-4AD739668620"
-    />
-    <Bookmark
-      title="
-Projeto: Interface Inclusiva - Maximizando Usabilidade e Acessibilidade para Todos"
-      subtitle="IFSC - 4ff92b43f6"
-      link="https://sigaa.ifsc.edu.br/sigaa/documentos"
-    />
-    <Bookmark
-      title="INCENTIVANDO O EMPREENDEDORISMO EM CAÇADOR/SC - 4A EDIÇÃO"
-      subtitle="IFSC - fdfd518c28"
-      link="https://sigaa.ifsc.edu.br/sigaa/documentos"
-    />
-    <Bookmark
-      title="Maratona do Zero ao Código - III Edição"
-      subtitle="Kenzie Academy Brasil"
-    />
-    <Bookmark
-      title="PYTHON 3 - MUNDO 1"
-      subtitle="Curso em vídeo - 4986E-66E2-5"
-      link="https://www.cursoemvideo.com/validacao-de-certificado/"
-    />
-    <Bookmark
-      title="PYTHON 3 - MUNDO 2"
-      subtitle="Curso em vídeo - 4986E-67AC-2"
-      link="https://www.cursoemvideo.com/validacao-de-certificado/"
-    />
-    <Bookmark
-      title="PYTHON 3 - MUNDO 3"
-      subtitle="Curso em vídeo - 4986E-685A-7"
-      link="https://www.cursoemvideo.com/validacao-de-certificado/"
-    />
-    <Bookmark
-      title="Padrões e Arquitetura de Software"
-      subtitle="PADIE do IFPR - 4059858.2256071.6.5.368969556437146"
-      link="https://www.even3.com.br//documentos"
-    />
+    <Column number-of-columns="2">
+      <template #column-1>
+        <div class="certificates__img-wrapper">
+          <img :src="['light-theme', 'cat-latte'].includes(theme) ? '/codecon-dark.webp' : '/codecon-light.webp'" />
+        </div>
+      </template>
+      <template #column-2>
+        <Text>Meetup Codecon Ladies</Text>
+        <br></br>
+        <Text class="highlight">Codecon</Text>
+        <br></br>
+        <Text>Credential ID 28332382.05162310.5.8.87727823659275368</Text>
+        <br></br>
+        <Link
+          emoji="🐈"
+          href="https://eventos.codecon.dev/documentos/imprimir?i=28332382.05162310.5.8.87727823659275368&cc=A6530560-1A59-43F5-B1E8-4AD739668620"
+          >Certificate</Link
+        >
+      </template>
+    </Column>
+
+    <Column number-of-columns="2">
+      <template #column-1>
+        <div class="certificates__img-wrapper">
+          <img src="/ifsc.webp" />
+        </div>
+      </template>
+      <template #column-2>
+        <Text>Projeto: Interface Inclusiva - Maximizando Usabilidade e Acessibilidade para Todos</Text>
+        <br></br>
+        <Text class="highlight">IFSC</Text>
+        <br></br>
+        <Text>Credential ID 4ff92b43f6</Text>
+        <br></br>
+        <Link
+          emoji="🐈"
+          href="https://sigaa.ifsc.edu.br/sigaa/documentos"
+          >Certificate</Link
+        >
+      </template>
+    </Column>
+
+     <Column number-of-columns="2">
+      <template #column-1>
+        <div class="certificates__img-wrapper">
+          <img src="/ifsc.webp" />
+        </div>
+      </template>
+      <template #column-2>
+        <Text>INCENTIVANDO O EMPREENDEDORISMO EM CAÇADOR/SC - 4A EDIÇÃO</Text>
+        <br></br>
+        <Text class="highlight">IFSC</Text>
+        <br></br>
+        <Text>Credential ID fdfd518c28</Text>
+        <br></br>
+        <Link
+          emoji="🐈"
+          href="https://sigaa.ifsc.edu.br/sigaa/documentos"
+          >Certificate</Link
+        >
+      </template>
+    </Column>
+
+      <Column number-of-columns="2">
+      <template #column-1>
+        <div class="certificates__img-wrapper">
+          <img src="/ifpr.png" />
+        </div>
+      </template>
+      <template #column-2>
+        <Text>Padrões e Arquitetura de Software</Text>
+        <br></br>
+        <Text class="highlight">PADIÊ IFPR</Text>
+        <br></br>
+        <Text>Credential ID 4059858.2256071.6.5.368969556437146</Text>
+        <br></br>
+        <Link
+          emoji="🐈"
+          href="https://www.even3.com.br//documentos"
+          >Certificate</Link
+        >
+      </template>
+    </Column>
   </div>
 </template>
 
@@ -64,5 +105,27 @@ Projeto: Interface Inclusiva - Maximizando Usabilidade e Acessibilidade para Tod
   display: flex;
   flex-direction: column;
   row-gap: 20px;
+
+  &__img-wrapper {
+    height: 110px;
+    width: 110px;
+    flex: 1;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .columns {
+    &__column:first-of-type {
+      display: flex;
+      flex: none;
+    }
+
+    &__column:nth-child(2) {
+      flex: none;
+    } 
+  }
 }
 </style>
