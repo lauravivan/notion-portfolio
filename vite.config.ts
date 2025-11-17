f25c4f5365ce0ee729d8529b12c66665a177a9ef
lauravivan <lauvivang@gmail.com>
2025-11-16 21:38:56 -0300

    fix: separate build files

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    dts({
-      include: ["../**/*.ts", "../**/*.vue"],
-      exclude: ["node_modules", "dist"],
-      tsconfigPath: path.resolve(__dirname, "./tsconfig.app.json"),
-      insertTypesEntry: true,
-    }),
-  ],
-
+  plugins: [vue()],
   resolve: {
     alias: [
       {
         find: "@core",
-        replacement: path.resolve(__dirname, "core")
-      }
-    ]
+        replacement: path.resolve(__dirname, "core"),
+      },
+    ],
   },
 
   css: {
@@ -31,26 +21,6 @@ export default defineConfig({
           @use "@core/assets/scss/_var.scss" as *;
           @use "@core/assets/scss/_mixin.scss" as *;
         `,
-      },
-    },
-  },
-
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    lib: {
-      entry: path.resolve(__dirname, "src/main.ts"),
-      name: "notion-portfolio",
-      fileName: (format) => `index.${format}.js`,
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: ["vue", "vue-router"],
-      output: {
-        globals: {
-          vue: "Vue",
-          "vue-router": "VueRouter",
-        },
       },
     },
   },
